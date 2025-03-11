79a2f9f4df8c42c699691a8940122a5b84ba17de
Giles Knap <hgv27681@p30-ioc-serv-01.diamond.ac.uk>
2025-03-11 14:35:42 +0000

    remove everything except me18x ioc

--- environment.sh
+++ environment.sh
@@ -30,16 +30,11 @@ else
 fi
 echo using $docker as container engine
 
-# ensure local container users can access X11 server
-xhost +SI:localuser:$(id -un)
-
 # Set up the environment for compose ###########################################
 
 # set user id for the phoebus container for easy X11 forwarding.
 export UIDGID=$USER_ID:$USER_GID
 # default to the test profile for docker compose
 export COMPOSE_PROFILES=test
-# for test profile our ca-gateway publishes PVS on the loopback interface
-export EPICS_CA_ADDR_LIST=127.0.0.1:5094
 # make a short alias for docker-compose for convenience
 alias dc='$docker compose'
